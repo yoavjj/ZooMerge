44d44d6136e62888660ddd3140a1cc8fd7218aae
yoavjj <yoav.jacques@gmail.com>
2025-12-31 11:20:20 +0200

    ui bug fix

--- Assets/ZooMerge/Script/PopupManager/PopupManager.cs
+++ Assets/ZooMerge/Script/PopupManager/PopupManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Linq;
 using UnityEngine;
 using static BallEventManager;
-using UnityEngine.iOS;
+//using UnityEngine.iOS;
 
 public class PopupManager : MonoBehaviour
 {
